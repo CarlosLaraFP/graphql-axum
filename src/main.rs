7e6d38d5e3fbc3e0fbd16d149174768f4879e310
CarlosLaraFP <info@carloslaraai.com>
2023-06-23 09:46:33 -0700

    GraphQL foundation

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,12 @@
-fn main() {
-    println!("Hello, world!");
+use axum::{Router, Server};
+
+#[tokio::main] // (1)
+async fn main() {
+    let app = Router::new(); // (2)
+    let address = "0.0.0.0:8000".parse().unwrap();
+
+    Server::bind(&address) // (3)
+        .serve(app.into_make_service())
+        .await
+        .unwrap();
 }
