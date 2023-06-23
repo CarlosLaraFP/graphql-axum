fb20d5a85d198d30f6bd3fdaa88f685bd4942279
CarlosLaraFP <info@carloslaraai.com>
2023-06-23 10:54:03 -0700

    GraphQL API implemented

--- src/main.rs
+++ src/main.rs
@@ -1,12 +1,24 @@
-use axum::{Router, Server, routing::get};
+use crate::model::QueryRoot;
+use crate::routes::{graphql_handler, graphql_playground, health};
+use async_graphql::{EmptyMutation, EmptySubscription, Schema};
+use axum::{extract::Extension, routing::get, Router, Server};
+mod model;
 mod routes;
-use crate::routes::health;
 
 #[tokio::main]
 async fn main() {
-    let app = Router::new().route("/health", get(health));
+    let schema = Schema::build(
+        QueryRoot,
+        EmptyMutation,
+        EmptySubscription
+    ).finish();
 
     let address = "0.0.0.0:8000".parse().unwrap();
+
+    let app = Router::new()
+        .route("/", get(graphql_playground).post(graphql_handler))
+        .route("/health", get(health))
+        .layer(Extension(schema));
 
     Server::bind(&address)
         .serve(app.into_make_service())
